6a1f3cc3e240c34c76f0e0084da5bb592e0345e4
Othou <mario.arriaga.peguero@hotmail.com>
2025-02-27 12:41:18 -0600

    agregar base

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base:"/<REPO>/",
   plugins: [
     VitePWA({
       registerType: "autoUpdate",
